c3552d208091259a9524816816984f1cb50e4d84
Nathan Beckmann <beckmann@mit.edu>
2011-07-16 00:56:56 -0400

    [graphs] Don't emit damage taken graphs.

--- graphs/Graphs.m
+++ graphs/Graphs.m
@@ -6,4 +6,4 @@ ext = ".png"
 
 plotDataSet[data, "Damage", 2, inputFile <> "-damage", ext]
 plotDataSet[data, "Heals", 3, inputFile <> "-heals", ext]
-plotDataSet[data, "Damage Taken", 4, inputFile <> "-damagetaken", ext]
+(* plotDataSet[data, "Damage Taken", 4, inputFile <> "-damagetaken", ext] *)
