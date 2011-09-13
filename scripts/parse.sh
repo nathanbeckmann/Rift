19ce2fee5a1a97abc1c40713684f3823a79a5624
Nathan Beckmann <beckmann@mit.edu>
2011-09-13 01:37:56 -0400

    [scripts] Don't output graph data by default.

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -2,4 +2,4 @@
 
 RIFT_HOME="../../RIFT Game/"
 
-cd parser && fsc *.scala && scala Simple "${RIFT_HOME}/CombatLog.txt" true true | tee "${RIFT_HOME}/Meters.txt"
+cd parser && fsc *.scala && scala Simple "${RIFT_HOME}/CombatLog.txt" true false | tee "${RIFT_HOME}/Meters.txt"
